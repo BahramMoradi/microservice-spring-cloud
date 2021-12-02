07f201053cc2a55a5865586a7ed392764cea82ff
Bahram Moradi <bahram.moradi@ufst.dk>
2021-12-02 15:55:49 +0100

    initial commit
    initial commit

---
+++ ms-spring-cloud/src/main/java/dk/ufst/bam/msspringcloud/MsSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package dk.ufst.bam.msspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringCloudApplication.class, args);
+	}
+
+}
---
+++ ms-spring-cloud/src/test/java/dk/ufst/bam/msspringcloud/MsSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ufst.bam.msspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
